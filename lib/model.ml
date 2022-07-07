db3790f556be1c73139ade568e1d9184a39f210d
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-07 12:14:20 -0400

    next function (R)

--- lib/model.ml
+++ lib/model.ml
@@ -23,12 +23,21 @@ type bindings = { acc: binding_map; cond_acc: binding_map }
 let recvCallback (binding : BindingRef.t) x =
   Session.Types.Recv (binding.cb, x)
 
-let respFor bindings ~binding cont : Session.Types.resp =
+let rec respFor bindings ~binding cont : Session.Types.resp =
   let cond = BindingRefMap.find binding bindings in
-  let specificCont = lazy (next bindings ~binding ~cond cont) in
-  let recv = Lazy.map (recvCallback binding) specificCont in
+  let specific_cont = lazy (next bindings ~binding ~cond cont) in
+  let recv = Lazy.map (recvCallback binding) specific_cont in
   (EventOccured binding), recv
 
-let next bindings ~binding ~cond cont =
+and next bindings ~binding ~cond cont =
   match cond with
-  | h :: t -> Session.Types.Branch 
+  | h :: t ->
+    let cont_prime = Session.RespSet.add (respFor bindings ~binding:h cont) cont in
+    let case_fun = next bindings ~binding ~cond:t in
+    let local_label x = CondAddBinding (x, h, binding) in
+    let responses =
+      [ local_label true, lazy (case_fun cont_prime)
+      ; local_label false, lazy (case_fun cont) ]
+    in
+    Session.Types.Branch (Session.RespSet.of_list responses)
+  | [] -> assert false
