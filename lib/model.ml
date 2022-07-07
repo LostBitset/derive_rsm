20c535e060d46274f9ef3590a08f9b41a21c421d
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-07 13:02:15 -0400

    stupid lazy problem should be fixed now

--- lib/model.ml
+++ lib/model.ml
@@ -18,26 +18,60 @@ module BindingRefMap = Map.Make(BindingRef)
 
 type binding_map = BindingRef.t list BindingRefMap.t
 
-type bindings = { acc: binding_map; cond_acc: binding_map }
+type bindings_t = { acc: binding_map; cond_acc: binding_map }
 
-let recvCallback (binding : BindingRef.t) x =
-  Session.Types.Recv (binding.cb, x)
+let recvCallback (binding : BindingRef.t) x : Session.Types.session =
+  Recv (binding.cb, x)
 
-let rec respFor bindings ~binding cont : Session.Types.resp =
-  let cond = BindingRefMap.find binding bindings in
-  let specific_cont = lazy (next bindings ~binding ~cond cont) in
-  let recv = Lazy.map (recvCallback binding) specific_cont in
+(*module SThunk = struct
+
+  type inner =
+    | KnownS of Session.RespSet.t
+    | ThunkS of BindingRef.t * inner
+
+  type t = InContext of bindings_t * inner
+
+  let apply bindings ~binding cont =
+    let thunk = ThunkS (binding, KnownS cont) in
+    InContext (bindings, thunk)
+
+  let force (InContext (bindings, x)) =
+    match x with
+    | KnownS set ->
+      set
+    | ThunkS (x, (ThunkS (y, _) as innermost)) as outermost ->
+      if x == y
+      then forceThunk bindings innermost
+      else forceThunk bindings outermost
+    | _ -> assert false
+
+  let rec forceThunk bindings (ThunkS (binding, arg)) =
+    let SnC
+    let get_acc x = Derivation.respFor bindings ~binding:x
+    let acc = List.map 
+
+end*)
+
+let (<<) f g x = f (g x)
+
+let rec respFor bindings ~binding (cont : Session.RespSet.t Lazy.t) =
+  let cond = BindingRefMap.find binding bindings.cond_acc in
+  let specific_cont x = next bindings ~binding ~cond x in
+  let recv = Lazy.map (recvCallback binding << specific_cont) cont in
   (EventOccured binding), recv
 
-and next bindings ~binding ~cond cont =
+and next bindings ~binding ~cond (cont : Session.RespSet.t) =
   match cond with
   | h :: t ->
-    let cont_prime = Session.RespSet.add (respFor bindings ~binding:h cont) cont in
+    let head = (respFor bindings ~binding:h (lazy cont)) in
+    let cont2 = Session.RespSet.add head cont in
     let case_fun = next bindings ~binding ~cond:t in
     let local_label x = CondAddBinding (x, h, binding) in
     let responses =
-      [ local_label true, lazy (case_fun cont_prime)
+      [ local_label true, lazy (case_fun cont2)
       ; local_label false, lazy (case_fun cont) ]
     in
     Session.Types.Branch (Session.RespSet.of_list responses)
   | [] -> assert false
+    (*let s_thunk = SThunk.apply bindings ~binding cont in
+    Select (Session.RespSet.map dualResp @@ SThunk.force s_thunk)*)
